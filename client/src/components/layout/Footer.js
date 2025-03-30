28c3534368bb2144ade34700b1896475741645df
Aradhana Kumari <aradhanakumari2602@gmail.com>
2025-03-30 23:03:05 +0530

    routing completed

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='bg-dark text-light p-4'>
+        <h6 className='text-center'>All Rights reserved &copy: techinfoyt</h6>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
