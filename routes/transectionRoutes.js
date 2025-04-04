b23b031e1b95224431203f767111f55e44c0c214
Aradhana Kumari <aradhanakumari2602@gmail.com>
2025-04-04 13:03:21 +0530

    added edit and delete functionalities

--- routes/transectionRoutes.js
+++ routes/transectionRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const {
   addTransection,
   getAllTransection,
+  editTransection,
+  deleteTransection,
+
 } = require("../controllers/transectionCtrl");
 
 //router object
@@ -10,6 +13,12 @@ const router = express.Router();
 //routes
 //add transection POST MEthod
 router.post("/add-transection", addTransection);
+
+//Edit transection POST MEthod
+router.post("/edit-transection", editTransection);
+
+//Delete transection POST MEthod
+router.post("/delete-transection", deleteTransection);
 
 //get transections
 router.post("/get-transection", getAllTransection);
